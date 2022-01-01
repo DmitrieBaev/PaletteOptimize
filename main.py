c206b8805d32431b693146fafba6ff3bd6c31b22
WhySoCupuyc <dm.baew@gmail.com>
2022-01-01 13:06:17 +0300

    quantization method added

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pathlib, sys, cv2, numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def quantization(img, k):
+    """Оптимизирует палитру изображения
+
+    :param img:
+    :param k: int
+        Количество цветов в новой палитре.
+    :return:
+        Изображение с оптимизированной палитрой.
+    """
+
+    data = np.float32(img).reshape((-1, 3))
+    # Внедряем метод k-средних
+    ret, label, center = cv2.kmeans(data, k, None,
+                                    (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001),
+                                    10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    return center[label.flatten()].reshape(img.shape)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    input_img = cv2.imread(pathlib.Path(sys.argv[1]))
+    output_img = quantization(input_img, int(sys.argv[2]))
+    cv2.imwrite('result.jpg', output_img)
